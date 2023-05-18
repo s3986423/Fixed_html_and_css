210482880be1ff9038bcd0353b2a06ba22044a8e
s3986423 <s3986423@rmit.edu.vn>
2023-05-18 12:33:55 +0700

    Fixed html and css
    plz read the README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Fixed_html_and_css
  open terminal in the current folder and run the command in README
+npm i
+node index.js
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3300
+
+app.set ('view engine','ejs')
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get ('/', (req,res) => {
+    res.render('index')
+});
+
+app.get ('/RegisterVendor', (req,res) => {
+    res.render('RegisterVendor')
+});
+
+app.get ('/SignUpVendor', (req,res) => {
+    res.render('SignUpVendor')
+});
+
+app.get ('/LogIn', (req,res) => {
+    res.render('LogIn')
+});
+
+app.get ('/About', (req,res) => {
+    res.render('About')
+});
+
+app.get ('/Helps', (req,res) => {
+    res.render('Helps')
+});
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`)
+});
